67f201e65183f77d15266b7e3047d3e96782eca3
sircoolio <ericklein@users.noreply.github.com>
2020-07-06 14:48:40 -0700

    code readability
    quick code update before branching parts for use on another project.

--- README.md
+++ README.md
@@ -1,80 +1,100 @@
 # clock
+## What is clock? 
+Clock that only turns on when an object (hand) is close to the device
 
-Description: Clock that only turns on when an object (hand) is close to the device
+### Purpose
+Clock template for other projects
 
-Sources
-  - uses Adafruit_LiquidCrystal https://github.com/adafruit/Adafruit_LiquidCrystal
-  - uses TimeLib https://github.com/PaulStoffregen/Time/blob/master/TimeLib.h
-  - uses DS1307RTC https://github.com/PaulStoffregen/DS1307RTC
-  - uses button https://github.com/ericklein/button
-  - uses VFDTube https://github.com/aguegu/nixie-tube
-    
-  Target
+### Contributors
+
+### Software Dependencies
+  - Adafruit_LiquidCrystal https://github.com/adafruit/Adafruit_LiquidCrystal
+  - TimeLib https://github.com/PaulStoffregen/Time/blob/master/TimeLib.h
+  - DS1307RTC https://github.com/PaulStoffregen/DS1307RTC
+  - button https://github.com/ericklein/button
+  - VFDTube https://github.com/aguegu/nixie-tube
+
+### BOM
     - Project requires 5v input
     - 6 VFD require dedicated 5v, >=1.3A
     - Works with all Arduino boards, uses I2C pins which are board dependent
-    - Overview of 16x2 LCD panels available at: http://oomlout.com/parts/LCDD-01-guide.pdf
-    - Overview of Adafruit i2c/SPI LCD Backpack at: https://learn.adafruit.com/i2c-spi-lcd-backpack/
-    - Overview of ChronoDot (RTC) at: http://docs.macetech.com/doku.php/chronodot_v2.0
-    - Overview of HC-SR04 (ultrasonic sensor) at: https://docs.google.com/document/d/1Y-yZnNhMYy7rwhAgyL_pfa39RsB-x2qR4vP8saG73rE/edit
 
-     * VFD moudle for Arduino
- *
- *              /----------------------------\
- *              |                            |
- *              |                            |
- *              |                            |
- *              \----------------------------/
- *               |                          |
- * ------------------------------------------------
- * | 5V | 5V | GND | GND | DIN | OE | STCP | SHCP |
- * ------------------------------------------------
- *
- * Two pairs of 5v/GND
- * one could be used for power in from external power source
- * the other one could be used as power for Arduino
 
- Information Sources
+### Pinouts
+
+
+### Information Sources
   - vfdtubes
     http://www.nixieclock.org/?p=411
     http://aguegu.net/?p=1334
     http://www.nixieclock.org/?p=464
     https://www.dfrobot.com/product-831.html
-    
-  Revisions
-  04/14/17 
-    - version cloned from current badge to control 6 pin LCD screen
-    - version cloned from current button though functionality is not used
-    - reading from ChronoDot via simple i2c read
-    - ChronoDot time set via example code not in this code base (yet?)
-  04/16/17
-    - button toggles screen text on and off, did not do what I wanted functionality wise
-  04/18/17
-    - Added ultrasonic code which will ultimately trigger clock visibility
-  04/23/17
-    - 041617 - put LCD backlight under programatic control
-    - 041817 - integrate ultrasonic into display, dependent on backlight control
-    - 041817 - move LCD to I2C
-    - button code removed
-  07/15/17
-    - 041417 - read RTC via time library (look at DS3232RTC.h and time.h)
-    - 070617 - move code in main to functions
-    - 042617 - single digit hours, minutes, seconds to 2 fixed digits
-    - 041817 - three digit seconds bug (due to cursor position and time length?) fix
-    - 041917 - untrasonic as range finding function returning cm as int
-  11/26/17
-    - 082017 - move comments to readme.md
-    - button support to library
-  06/21/18
-    - 062018 - conditional switch between LCD and VFDTube displays
-    - improved clock string handling = ClockDigits(x) removed
-  07/16/18
-    - 071617 - ability to modify time
-    - 062018 - ability to set time manually
-    - 071618 - changes for ButtonHandler library updates
 
-  Feature Requests
-    - 042317 - move serial debug code to conditional compile
-  	- 042617 - supress power and built-in LEDs on development boards
-  	- 050417 - change screen types to facilitate larger text for time
-  	- 050417 - ability to change LCD backlight level
+  - Overview of 16x2 LCD panels available at: http://oomlout.com/parts/LCDD-01-guide.pdf
+  - Overview of Adafruit i2c/SPI LCD Backpack at: https://learn.adafruit.com/i2c-spi-lcd-backpack/
+  - Overview of ChronoDot (RTC) at: http://docs.macetech.com/doku.php/chronodot_v2.0
+  - Overview of HC-SR04 (ultrasonic sensor) at: https://docs.google.com/document/d/1Y-yZnNhMYy7rwhAgyL_pfa39RsB-x2qR4vP8saG73rE/edit
+
+#### VFD Arduino module
+
+              /----------------------------\
+              |                            |
+              |                            |
+              |                            |
+              \----------------------------/
+               |                          |
+ ------------------------------------------------
+ | 5V | 5V | GND | GND | DIN | OE | STCP | SHCP |
+ ------------------------------------------------
+
+Two pairs of 5v/GND, one could be used for power in from external power source, the other one could be used as power for Arduino
+
+### Issues
+070720 - Code needs to be scrubbed for instances where LCD (row) needs to be cleared before new output
+070720 - fix issue where RTC and system time are not being set if RTC has no time stored
+
+### Questions
+050417 - are there LCDs with larger character height to facilitate larger text
+042617 - how to supress power and built-in LEDs on development boards
+
+### Learnings
+
+### Feature Requests
+050417 - ability to change LCD backlight level
+070620 - optimize adjustTime to remove redundant code
+ 
+### Revisions
+041417 
+  - version cloned from current badge to control 6 pin LCD screen
+  - version cloned from current button though functionality is not used
+  - reading from ChronoDot via simple i2c read
+  - ChronoDot time set via example code not in this code base (yet?)
+041617
+  - button toggles screen text on and off, did not do what I wanted functionality wise
+041817
+  - Added ultrasonic code which will ultimately trigger clock visibility
+042317
+  - 041617 - put LCD backlight under programatic control
+  - 041817 - integrate ultrasonic into display, dependent on backlight control
+  - 041817 - move LCD to I2C
+  - button code removed
+071517
+  - 041417 - read RTC via time library (look at DS3232RTC.h and time.h)
+  - 070617 - move code in main to functions
+  - 042617 - single digit hours, minutes, seconds to 2 fixed digits
+  - 041817 - three digit seconds bug (due to cursor position and time length?) fix
+  - 041917 - untrasonic as range finding function returning cm as int
+112617
+  - 082017 - move comments to readme.md
+  - button support to library
+062118
+  - 062018 - conditional switch between LCD and VFDTube displays
+  - improved clock string handling = ClockDigits(x) removed
+071618
+  - 071617 - ability to modify time
+  - 062018 - ability to set time manually
+  - 071618 - changes for ButtonHandler library updates
+070720
+  - general cleanup work for readability
+  - [FR]042317 - move serial debug code to conditional compile
+  - debug messages to LCD if available
--- clock.ino
+++ clock.ino
@@ -1,133 +1,160 @@
 /*
-  Project Name:   clock
-  Developer:      Eric Klein Jr. (temp2@ericklein.com)
-  Description:    Clock that only turns on when an object (hand) is close to the device
+  Project Name:	clock
+  Developer:	Eric Klein Jr. (temp2@ericklein.com)
+  Description:	template for clock projects
 
   See README.md for target information, revision history, feature requests, etc.
 */
 
-// Library initialization
-#include <Adafruit_LiquidCrystal.h>
-#include <Wire.h>
-#include <TimeLib.h>
-#include <DS1307RTC.h> // Works with DS3231 as well
-#include "buttonhandler.h"
-#include "VFDTube.h"
-
-// Conditional code, comment out the display not in use
+// Conditional (hardware functionality)
 // #define displayVFD
 #define displayLCD
+//#define buttonEnabled
+//#define DEBUG 			// debug messages to serial port
 
-//Pin connections
+#ifdef displayLCD
+	#define EXT_DEBUG	// debug messages to external LCD display
+#endif
+
+// time library initialization
+#include <TimeLib.h>
+#include <Wire.h>
+#include <DS1307RTC.h> // Works with DS3231 as well
+
+//Hardware setup
 //buttons
-#define pushButtonHourPin  12
-#define pushButtonMinutePin  11
+#ifdef buttonEnabled
+	#include "buttonhandler.h"
+	#define pushButtonHourPin		12
+	#define pushButtonMinutePin		11
+	#define longButtonPressDelay	3000
+	enum { BTN_NOPRESS = 0, BTN_SHORTPRESS, BTN_LONGPRESS };
+	ButtonHandler buttonHour(pushButtonHourPin, longButtonPressDelay);
+	ButtonHandler buttonMinute(pushButtonMinutePin, longButtonPressDelay);
+#endif
 
 // ultrasound
-#define trigPin 7
+#define triggerPin 7
 #define echoPin 6
+const byte triggerDistance = 20;  // Distance in cm to toggle LCD backlight
 
 #ifdef displayVFD
-#define DINPin  8
-#define OEPin   9 //PWM output
-#define STCPPin 10
-#define SHCPPin 11
+	#include "VFDTube.h"
+	#define DINPin  8
+	#define OEPin   9 //PWM output
+	#define STCPPin 10
+	#define SHCPPin 11
+	#define tubeCount 6
+	VFDTube tube(DINPin, OEPin, STCPPin, SHCPPin, tubeCount);
+	tube.setBrightness(0x4b); // set brightness, range 0x00 - 0xff [0-255]
+#else 
+	// LCD display
+	#include <Adafruit_LiquidCrystal.h>
+	#define lcdRows   2
+	#define lcdColumns 16
+	Adafruit_LiquidCrystal lcd(0); //i2c connection, default address #0 (A0-A2 not jumpered)
 #endif
 
-// Global variables
-const byte triggerDistance = 20;  // Distance in centimeters to turn on and off the LCD backlight
-enum { BTN_NOPRESS = 0, BTN_SHORTPRESS, BTN_LONGPRESS };
-#define buttonDelay 10
-#ifdef displayVFD
-#define tubeCount 6
-#endif
+void setup()
+{
+	#ifdef DEBUG
+		Serial.begin(115200);
+		while (!Serial) 
+	  	{
+	    	delay(1);
+	  	}
+	#endif
 
+	//Setup ultrasonic sensor
+	pinMode(triggerPin, OUTPUT);
+	pinMode(echoPin, INPUT);
+
+  	//Setup push buttons
+	#ifdef buttonEnabled
+		buttonHour.init();
+		buttonMinute.init();
+  	#endif
+
+	// make sure the RTC has been set previously
+	setSyncProvider(RTC.get);
+  	if (timeStatus() != timeSet)
+  	// Issues 070720: check that elapsed t isn't near zero, aka RTC is awake but isn't set
+  	{
+		setTime(23,14,00,07,7,2020); //values in the order hr,min,sec,day,month,year
+		RTC.set(now());
+		#ifdef DEBUG
+    		Serial.println("Unable to sync with RTC, time was set via code default");
+    	#endif
+    	#ifdef EXT_DEBUG
+      		displayLCDMessage(0, "Time set in code");
+    	#endif
+	}
+  	else
+    {
+		#ifdef DEBUG
+    		Serial.println("Time provided via RTC");
+    	#endif
+    	#ifdef EXT_DEBUG
+      		displayLCDMessage(0, "RTC set time");
+      		delay(2000);
+    	#endif
+	}
+
+// Prepare LCD
 #ifdef displayLCD
-#define lcdRows   2
-#define lcdColumns 16
-#endif
-
-// Display connections
-#ifdef displayLCD
-Adafruit_LiquidCrystal lcd(0); //i2c connection, default address #0 (A0-A2 not jumpered)
-#endif
-#ifdef displayVFD
-VFDTube tube(DINPin, OEPin, STCPPin, SHCPPin, tubeCount);
-#endif
-
-// Instantiate button objects
-ButtonHandler buttonHour(pushButtonHourPin, buttonDelay);
-ButtonHandler buttonMinute(pushButtonMinutePin, buttonDelay);
-
-void setup() {
-  Serial.begin(115200);
-
-  //Setup ultrasonic sensor
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-
-  //Setup push buttons
-  buttonHour.init();
-  buttonMinute.init();
-
-  while (!Serial) ; // wait until Arduino Serial Monitor opens
-
-      // make sure the RTC has been set previously
-      setSyncProvider(RTC.get);
-      if (timeStatus() != timeSet)
-      {
-        Serial.println("Unable to sync with RTC, time needs to be manually updated");
-        setTime(18,51,00,22,6,2018); //values in the order hr,min,sec,day,month,year
-      }
-      else
-        Serial.println("RTC has set the time");
-
-  // Prepare LCD
-  #ifdef displayLCD
-    lcd.begin(lcdColumns, lcdRows);
-    lcd.print("Current time is:");
-  #endif
-
-  // Prepare vfd
-#ifdef displayVFD
-  tube.setBrightness(0x4b); // set brightness, range 0x00 - 0xff [0-255]
+	lcd.begin(lcdColumns, lcdRows);
+	displayLCDMessage(0,"Current time is:");
 #endif
 }
 
 void loop()
 {
-  adjustTime();
-  if (timeStatus() == timeSet)
+  #ifdef buttonEnabled
+  	adjustTime();
+  #endif
+  //if (timeStatus() == timeSet)
+  if (timeSet)
   {
     displayClock();
     toggleBacklight(readDistance());
   } 
-  else Serial.println("RTC is either waiting to sync or a serious hardware issue has emerged");
+  else
+  {
+  	#ifdef DEBUG
+		Serial.println("RTC is either waiting to sync or a serious hardware issue has emerged");
+	#endif
+	#ifdef EXT_DEBUG
+  		displayLCDMessage(1, "RTC wait to sync");
+	#endif
+  }
 }
 
 int readDistance ()
 {
-  long duration;
-  int  cm;
+    // Returns distance from sensor in centimeters
+	long duration;
+	int  cm;
 
-  // Returns distance from sensor in centimeters
-  // ultrasonic sensor read
-  // clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 microseconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Distance = (Speed of sound * Time delay) / 2
-  // the speed of sound is 343.4 m/s or 0.0343 cm/microsecond to the Temperature of 20°C.
-  //   inches = (duration/2) / 741;
-  cm = duration / 58;
-  //Serial.print(cm);
-  //Serial.print(" cm; ");
-  return (cm);
+	// ultrasonic sensor read
+	// clears the triggerPin
+	digitalWrite(triggerPin, LOW);
+	delayMicroseconds(2);
+	// Sets the triggerPin on HIGH state for 10 microseconds
+	digitalWrite(triggerPin, HIGH);
+	delayMicroseconds(10);
+	digitalWrite(triggerPin, LOW);
+	// Reads the echoPin, returns the sound wave travel time in microseconds
+	duration = pulseIn(echoPin, HIGH);
+	// Distance = (Speed of sound * Time delay) / 2
+	// the speed of sound is 343.4 m/s or 0.0343 cm/microsecond to the Temperature of 20°C.
+	// inches = (duration/2) / 741;
+	cm = duration / 58;
+	#ifdef DEBUG
+		Serial.print("Distance to object is ");
+		Serial.print(cm);
+		Serial.println(" cm");
+	#endif
+	return (cm);
 }
 
 void toggleBacklight (int distance)
@@ -135,87 +162,104 @@ void toggleBacklight (int distance)
   // Clock display is made visible if object is close enough to clock
   if (distance < triggerDistance)
   {
-    //lcd
-#ifdef displayLCD
-    lcd.setBacklight(HIGH);
-#endif
-    //vfd
-#ifdef displayVFD
-    tube.setBrightness(0xd0); // set brightness, range 0x00 - 0xff [0-255]
-#endif
-    //Serial.println("activating backlight");
-
+	#ifdef displayVFD
+		tube.setBrightness(0xd0); // set brightness, range 0x00 - 0xff [0-255]
+	#else
+		lcd.setBacklight(HIGH);
+	#endif
+	#ifdef DEBUG
+	Serial.println("Backlight activated");
+	#endif
   }
   else
   {
-    //lcd
-#ifdef displayLCD
-    lcd.setBacklight(LOW);
-#endif
-    //vfd
-#ifdef displayVFD
-    tube.setBrightness(0x4b); // set brightness, range 0x00 - 0xff [0-255]
-#endif
-    //Serial.println("deactiving backlight");
+	#ifdef displayVFD
+		tube.setBrightness(0x4b); // set brightness, range 0x00 - 0xff [0-255]
+	#else
+		lcd.setBacklight(LOW);
+	#endif
+	#ifdef DEBUG
+	Serial.println("Backlight de-activated");
+	#endif
   }
 }
 
 void displayClock()
 {
-  char timebuffer[9];
-  sprintf(timebuffer, "%02d:%02d:%02d", hour(), minute(), second());
-  // LCD
-#ifdef displayLCD
-  lcd.setCursor(0, 1); // column 0, line 1. line 1 is the second row, since counting begins with 0
-  lcd.print(timebuffer);
-#endif
+	char timebuffer[9];
+	sprintf(timebuffer, "%02d:%02d:%02d", hour(), minute(), second());
 
-  // vfd
-#ifdef displayVFD
-  tube.clear();
-  tube.printf("%02d%02d%02d", hour(), minute(), second());
-  tube.display();
-#endif
-
-  // debug time display
-  //Serial.println(timebuffer);
+	#ifdef displayVFD
+		tube.clear();
+		tube.printf("%02d%02d%02d", hour(), minute(), second());
+		tube.display();
+	#else
+		lcd.setCursor(0, 1); // column 0, line 1. line 1 is the second row, since counting begins with 0
+		lcd.print(timebuffer);
+	#endif
 }
 
+#ifdef buttonEnabled
 void adjustTime()
 // Manually update time via user input
 {
-  int newHour = hour();
-  int newMinute = minute();
-  switch (buttonHour.handle())
-  {
-  case BTN_SHORTPRESS:
-    //Debug text
-    Serial.println("hour button short press");
-    if (newHour == 23) newHour=0;
-    else newHour++;
-    setTime(newHour,minute(), second(), day(), month(), year());
-  if (RTC.chipPresent()) RTC.set(now());
-  else
-      Serial.println("Unable to set RTC time");
-  break;
-  case BTN_LONGPRESS:
-    Serial.println("hour button long press"); //debug text
-    break;
-  }
-
-  switch (buttonMinute.handle())
-  {
-  case BTN_SHORTPRESS:
-  Serial.println("minute button short press"); //debug text
-    if (newMinute == 59) newMinute=0;
-    else newMinute++;
-        setTime(hour(),newMinute, second(), day(), month(), year());
-    if (RTC.chipPresent()) RTC.set(now());
-    else
-      Serial.println("Unable to set RTC time");  
-  break;
-  case BTN_LONGPRESS:
-    Serial.println("minute button long press"); //debug text
-    break;
-  }
+	int newHour = hour();
+	int newMinute = minute();
+	switch (buttonHour.handle())
+	{
+		case BTN_SHORTPRESS:
+		//Debug text
+			#ifdef DEBUG
+				Serial.println("hour button short press");
+			#endif
+			if (newHour == 23) 
+				newHour = 0;
+			else newHour++;
+			setTime(newHour,minute(), second(), day(), month(), year());
+			if (RTC.chipPresent()) 
+				RTC.set(now());
+			else
+				#ifdef DEBUG
+					Serial.println("unable to set RTC time");
+				#endif
+		break;
+		case BTN_LONGPRESS:
+			#ifdef DEBUG
+				Serial.println("hour button long press");
+			#endif
+		break;
+	}
+	switch (buttonMinute.handle())
+	{
+		case BTN_SHORTPRESS:
+			#ifdef DEBUG
+				Serial.println("minute button short press");
+			#endif
+			if (newMinute == 59)
+				newMinute = 0;
+			else newMinute++;
+	    	setTime(hour(),newMinute, second(), day(), month(), year());
+			if (RTC.chipPresent())
+				RTC.set(now());
+			else
+	  			#ifdef DEBUG
+					Serial.println("unable to set RTC time");
+				#endif  
+		break;
+		case BTN_LONGPRESS:
+			#ifdef DEBUG
+				Serial.println("minute button long press");
+			#endif
+		break;
+	}
 }
+#endif
+
+#ifdef EXT_DEBUG
+  void displayLCDMessage(int row, String message)
+  // first row is zero
+  {
+    lcd.setCursor(0, row);
+    lcd.print(message);
+  }
+#endif
